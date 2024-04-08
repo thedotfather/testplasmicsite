70677684eb686bc2e30e87585638998f73f68053
thedotfather <admin@thedotfather.com>
2024-04-08 18:26:22 +0200

    est

--- components/MUI_X_Data_Grid_MIT.tsx
+++ components/MUI_X_Data_Grid_MIT.tsx
@@ -17,7 +17,7 @@ const DataGridDemo: React.FC<DataGridDemoProps> = ({
   const dataGridProps: any = {
     rows,
     columns,
-    pageSizeOptions: [5, 50, 100],
+    pageSizeOptions :Array<number>,
     checkboxSelection: true,
     
   };
