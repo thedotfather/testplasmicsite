26bf6cbae1507b42c3ca60721564e662a1b22663
thedotfather <admin@thedotfather.com>
2024-04-17 18:08:34 +0200

    t

--- components/MUI_X_Data_Grid_MIT.tsx
+++ components/MUI_X_Data_Grid_MIT.tsx
@@ -101,7 +101,8 @@ const DataGridDemo: React.FC<DataGridDemoProps> = ({
 
       for (const action of actions) {
         try {
-          const iconModule = await import(`@mui/icons-material/${action.iconType}`).catch(() => ({ default: defaultIcon }));
+          const iconPath = '@mui/icons-material/' + action.iconType;
+          const iconModule = await import(iconPath).catch(() => ({ default: defaultIcon }));
           newIcons[action.iconType] = iconModule.default;
         } catch (error) {
           console.error(`Error loading icon ${action.iconType}:`, error);
